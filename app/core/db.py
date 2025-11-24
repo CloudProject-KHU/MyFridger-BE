d09698b2ad1ed47191b90a0b7305d0dd61996984
Su-Yong <simssy2205@gmail.com>
2025-11-24 23:16:20 +0900

    fix(aws): 백엔드 배포 오류 수정

--- app/core/db.py
+++ app/core/db.py
@@ -1,3 +1,4 @@
+import ssl
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import create_async_engine
@@ -6,8 +7,16 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 
 from .config import settings
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True, future=True)
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
 
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=True,
+    future=True,
+    connect_args={"ssl": ssl_context} if settings.ENVIRONMENT == "production" else {},
+)
 
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
